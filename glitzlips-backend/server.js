df1849d3b2f9042089e602ec8819756df0c733f0
Cherlynn1 <cherlynnfreeman@gmail.com>
2024-05-25 09:24:10 -0600

    Remove Stripe secret key from server.js and use environment variable

---
+++ glitzlips-backend/server.js
@@ -0,0 +1,25 @@
+// server.js
+
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/charge', async (req, res) => {
+    const { stripeToken } = req.body;
+    const charge = await stripe.charges.create({
+        amount: 1999, // Amount in cents
+        currency: 'usd',
+        description: 'Example charge',
+        source: stripeToken,
+    });
+    res.send('Success');
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(`Server is running on port ${process.env.PORT || 3000}`));
