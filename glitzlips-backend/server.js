e565c390c07aacb26aad5cecbaddc70fcb32136c
Cherlynn1 <cherlynnfreeman@gmail.com>
2024-05-25 09:58:16 -0600

    Clean up untracked files and update .gitignore

--- glitzlips-backend/server.js
+++ glitzlips-backend/server.js
@@ -22,4 +22,5 @@ app.post('/charge', async (req, res) => {
     res.send('Success');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log(`Server is running on port ${process.env.PORT || 3000}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
